6a271ea77e2882eab3a0f8f77ce0a1f260113df8
1b1930 <danpsr1964@gmail.com>
2021-11-10 11:45:39 -0300

    método lerCsv agora lê todos os elementos

--- ArquivoOps.java
+++ ArquivoOps.java
@@ -2,32 +2,22 @@ import java.io.FileReader;
 import java.util.Arrays;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+// Essa classe contém todos os métodos que fazem operações com arquivos
 public class ArquivoOps {
 
-    static String[] lerCsv(File caminhoCsv) {
-        String[] dadosCsv = {""};
-        try {
-            // funcionou??? pelo menos um tico...
-            BufferedReader _lerCsv = new BufferedReader(new FileReader(caminhoCsv));
-            String row;
-            while ((row = _lerCsv.readLine()) != null) {
-                dadosCsv = row.split(",");
-                // do something with the data
-            }
-            _lerCsv.close();
-        } catch(Exception e) {
-            System.out.println("Arquivo CSV não existe, fechando o programa...");
-            System.exit(0);
+    static void lerCsv(File caminhoCsv) throws FileNotFoundException {
+        // OK, esse funciona direito. Lê todas as linhas e colunas do arquivo csv
+        // TODO: Botar esses dados num ArrayList (2d?) para poderem ser manipulados ou printados
+        Scanner scanner = new Scanner(caminhoCsv);
+        scanner.useDelimiter(",");
+
+        while (scanner.hasNext()) {
+            System.out.println(scanner.next());
         }
-
-        for (String str : dadosCsv) {
-            System.out.println(str);
-        }
-
-        return(dadosCsv);
-
+        scanner.close();
     }
 
 }
