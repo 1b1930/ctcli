fb9734ec9ad17620d9de1a6ca61d43320af2d20e
1b1930 <danpsr1964@gmail.com>
2021-11-10 15:03:01 -0300

    Mais migrações para OpenCSV

--- ArquivoOps.java
+++ ArquivoOps.java
@@ -1,11 +1,16 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 // Essa classe contém todos os métodos que fazem operações com arquivos
@@ -17,19 +22,29 @@ public class ArquivoOps {
  
         // Criar um objeto da classe FileReader
         // com o arquivo .csv como parâmetro
-        FileReader leitorDeArquivo = new FileReader(arq);
+        FileReader fileReader = new FileReader(arq);
  
         // Criar um objeto da classe CSVReader 
-        // file reader as a parameter
-        CSVReader leitorCsv = new CSVReader(leitorDeArquivo);
+        // usa fileReader como parâmetro
+        CSVReader leitorCsv = new CSVReader(fileReader);
         String[] proximaEntrada;
- 
-        // we are going to read data line by line
+        // Controle de iteração pra consertar um probleminha estético no loop a seguir
+        int it = 0;
+        // Lendo os dados linha por linha
         while ((proximaEntrada = leitorCsv.readNext()) != null) {
-            for (String cell : proximaEntrada) {
-                System.out.print(cell + "\t");
+
+            for (int i=0;i<proximaEntrada.length; i++) {
+                String cell = proximaEntrada[i];
+                if (i==proximaEntrada.length-1 && it == 1) {
+                    System.out.print("\t\t" + cell);
+                } else {System.out.print(cell + "\t");}
+                
             }
+//            for (String cell : proximaEntrada) {
+//                System.out.print(cell + "\t");
+//            }
             System.out.println();
+            it++;
         }
     }
     catch (Exception e) {
@@ -38,25 +53,41 @@ public class ArquivoOps {
 }
 
 
-    /* TODO: Obviamente modificar essa função pra ser dinâmica e atender as necessidades do programa. */
-    void escreverCsv() {
-        PrintWriter pw = null;
+    void escreverDadosLinhaPorLinha(String caminhoArq, String nome, String peso, String altura, String nivelatv)
+    {
+
+        // Cria objeto da classe File usando como parâmetro o caminho do arquivo csv
+        File file = new File(caminhoArq);
         try {
-            pw = new PrintWriter(new File("/mnt/hdd/code/DadosUsuario.csv"));
-        } catch (FileNotFoundException e) {
+            // Cria objeto da classe FileWriter com file como parâmetro
+            FileWriter outputfile = new FileWriter(file);
+    
+            // Cria objeto da classe CSVWriter com objeto da classe FileWriter como parâmetro
+            CSVWriter writer = new CSVWriter(outputfile);
+
+            // Pegando a data atual para poder adicionar ao csv
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+            LocalDateTime horaAgora = LocalDateTime.now();  
+            System.out.println(dtf.format(horaAgora));
+    
+            // adicionando cabeçalho ao csv
+            String[] header = { "Nome", "Peso", "Altura", "Nível de Atividade", "Última Atualização" };
+            writer.writeNext(header);
+    
+            // adicionando dados ao csv
+            // TODO: Descobrir como editar dados e adicionar dados sem reescrever todo o arquivo
+            String[] data1 = { nome, peso, altura, nivelatv, dtf.format(horaAgora) };
+            writer.writeNext(data1);
+//            String[] data2 = { "Suraj", "10", "630" };
+//            writer.writeNext(data2);
+    
+            // closing writer connection
+            writer.close();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        StringBuilder builder = new StringBuilder();
-        // A última coluna, DataAtt, refere-se a última vez que os dados foram atualizados.  
-        String nomeColunas = "UsrId,Nome,Peso,Altura,NivelAtividade,DataAtt";
-        // No need give the headers Like: id, Name on builder.append
-        builder.append(nomeColunas +"\n");
-        builder.append("1"+",");
-        builder.append("Daniel");
-        builder.append('\n');
-        pw.write(builder.toString());
-        pw.close();
-        System.out.println("CSV escrito.");
     }
 
 }
--- Main.java
+++ Main.java
@@ -1,19 +1,20 @@
-import java.io.File;
+
 import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         // TODO: Criar um .csv pessoal para cada usuário com os alimentos consumidos? Nem sei como faz isso
         System.out.println("fucktest");
-        File csv = new File("/mnt/hdd/code/DadosUsuario.csv");
+//        File csv = new File("/mnt/hdd/code/DadosUsuario.csv");
+        final String CAMINHOARQUIVO = "/mnt/hdd/code/DadosUsuario.csv";
 
         // Cria instância da classe ArquivoOps
         ArquivoOps arquivoOps = new ArquivoOps();
 
 //        arquivoOps.lerCsv(csv);
+        arquivoOps.escreverDadosLinhaPorLinha(CAMINHOARQUIVO, "Daniel", "190", "170", "1" );;
         arquivoOps.lerDadosLinhaPorLinha("/mnt/hdd/code/DadosUsuario.csv");
-        arquivoOps.escreverCsv();
 
-        InterfaceCLI intf = new InterfaceCLI();
+//        InterfaceCLI intf = new InterfaceCLI();
 //        intf.mostrar();
 
 
