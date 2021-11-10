55e0a667ceb84f70fb67d46806edf223f8b513a4
1b1930 <danpsr1964@gmail.com>
2021-11-10 11:46:11 -0300

    começo da escrita da tela de boas vindas

--- InterfaceCLI.java
+++ InterfaceCLI.java
@@ -1,11 +1,22 @@
 public class InterfaceCLI {
     
     void mostrar() {
-        System.out.println("Bem vindo ao ctcli.");
-        System.out.println("Esse aplicativo irá lhe ajudar a contar suas calorias");
+        // Caracteres especiais ANSI para fazer o texto ficar em negrito
+        System.out.println("\033[0;1m");
+        // mensagem de boas vindas + explicação do app
+        System.out.println("Bem vindo ao ctcli.\n");
+        System.out.println("Esse aplicativo irá lhe ajudar a contar suas calorias.");
+        System.out.println("Ele também informa o seu valor total de energia gasta por dia" +
+        " " + "(sigla em inglês: TDEE)");
+        System.out.println("O TDEE é importante pelo fato de ");
     }
 
-    
+    class MenuPrincipal {
+        void mostrar() {
+            System.out.println("");
+        }
+    }
+
 
 
 
