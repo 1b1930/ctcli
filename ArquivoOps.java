37843fe9168f41b6eea86a1b8810082d93ad9fb5
1b1930 <danpsr1964@gmail.com>
2021-11-10 12:08:40 -0300

    Inicío da escrita das operações de escrita para .csv

--- ArquivoOps.java
+++ ArquivoOps.java
@@ -1,14 +1,30 @@
-import java.io.FileReader;
-import java.util.Arrays;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 // Essa classe contém todos os métodos que fazem operações com arquivos
 public class ArquivoOps {
 
-    static void lerCsv(File caminhoCsv) throws FileNotFoundException {
+    public String converterParaCsv(String[] dado) {
+        return Stream.of(dado)
+            .map(this::escaparCaracteresEspeciais)
+            .collect(Collectors.joining(","));
+    }
+
+    public String escaparCaracteresEspeciais(String dado) {
+        String dadoEscapado = dado.replaceAll("\\R", " ");
+        if (dado.contains(",") || dado.contains("\"") || dado.contains("'")) {
+            dado = dado.replace("\"", "\"\"");
+            dadoEscapado = "\"" + dado + "\"";
+        }
+        return dadoEscapado;
+    }
+
+
+
+    void lerCsv(File caminhoCsv) throws FileNotFoundException {
         // OK, esse funciona direito. Lê todas as linhas e colunas do arquivo csv
         // TODO: Botar esses dados num ArrayList (2d?) para poderem ser manipulados ou printados
         Scanner scanner = new Scanner(caminhoCsv);
