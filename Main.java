25bff256567963d726a0d177978ccde0720ff0bc
1b1930 <danpsr1964@gmail.com>
2021-11-10 11:47:17 -0300

    teste de commit 1

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        // TODO: Criar um .csv pessoal para cada usuário com os alimentos consumidos? Nem sei como faz isso
         System.out.println("fucktest");
         File csv = new File("/mnt/hdd/code/test1.csv");
         ArquivoOps.lerCsv(csv);
+
+        InterfaceCLI intf = new InterfaceCLI();
+        intf.mostrar();
+
+
         
     }
 }
