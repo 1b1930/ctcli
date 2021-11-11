29187b4729cc7df272b5aacc7b1abfe24390ab61
1b1930 <danpsr1964@gmail.com>
2021-11-11 11:36:14 -0300

    Add: CLIUtil - comentários

--- CLIUtil.java
+++ CLIUtil.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
 
 public class CLIUtil {
+    // Método que cuida de pegar os dados entrados pelo usuário
     static String getUserInput() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("\033[0;4m>>\033[0;0m ");
         String str = scanner.nextLine();
         str.replace("\n", "");
         if (!str.trim().isEmpty() && str != "") {
-            // System.out.println("string isn't null or only white spaces!");
-            // scanner.close();
-            // return str;
+
         } else {
-            System.out.println("Invalid command.");
+            System.out.println("Comando inválido.");
             CLIUtil.getUserInput();
         }
         // scanner.close();
