d5e967b4db3962a5c2b85f155ea09e2da977f795
1b1930 <danpsr1964@gmail.com>
2021-11-13 10:49:41 -0300

    fix erro no scanner: NoSuchElementException

--- CLIUtil.java
+++ CLIUtil.java
@@ -1,27 +1,38 @@
 import java.util.Scanner;
 
 public class CLIUtil {
+
+    // Segundo um cara no stackoverflow, scanners usados em múltiplos lugares
+    // devem ficar fora de um método e ter como características public static e final
+    // Funcionou por agora...
+    public static final Scanner scanner = new Scanner(System.in);
+
     // Método que cuida de pegar os dados entrados pelo usuário
     static String getUserInput() {
-        Scanner scanner = new Scanner(System.in);
         System.out.print("\033[0;4m>>\033[0;0m ");
-        String str = scanner.nextLine();
-        str.replace("\n", "");
-        if (!str.trim().isEmpty() && str != "") {
+        if(scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            str.replace("\n", "");
 
-        } else {
-            System.out.println("Comando inválido.");
-            CLIUtil.getUserInput();
+            if (!str.trim().isEmpty() && str != "") {
+
+            } else {
+                System.out.println("Comando inválido.");
+                CLIUtil.getUserInput();
+            }
+            return str;
+
         }
-        // scanner.close();
-        return str;
+        return "aaa";
+
+
 
 
     }
 
     // Esperar pela próxima nova linha antes de continuar
     static void waitNext() {
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
         // scanner.close();
     }
