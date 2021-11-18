ac0e8bf9782523929445f18ea889655b2cbdf698
1b1930 <danpsr1964@gmail.com>
2021-11-18 14:35:54 -0300

    add: CLIUtil - getDataHora e printFileDirList

--- CLIUtil.java
+++ CLIUtil.java
@@ -1,4 +1,14 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class CLIUtil {
 
@@ -27,5 +37,29 @@ public class CLIUtil {
         // Sempre use println e não print quando usar esse caractere especial
         // Se usar print(), dá erro depois de um tempo.
         System.out.println("\033[H\033[2J");
+    }
+
+    static String getDataHora() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");  
+        LocalDateTime now = LocalDateTime.now();  
+        return(dtf.format(now).toString());
+
+    }
+
+    static void printFileDirList(String dir) {
+        try {
+            List<File> files = Files.list(Paths.get(dir))
+            .filter(Files::isRegularFile)
+            .map(Path::toFile)
+            .collect(Collectors.toList());
+            //return files;
+            files.forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 }
