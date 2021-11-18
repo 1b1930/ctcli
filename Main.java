56aa54e3dcd7e980300d769ca6f133af207c854f
1b1930 <danpsr1964@gmail.com>
2021-11-18 14:36:24 -0300

    adicionar estrutura de arquivos da versão final

--- Main.java
+++ Main.java
@@ -1,16 +1,26 @@
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
     // constantes com os caminhos para os arquivos csv
     // TODO: botar os CSVs dentro da pasta do programa
 
-    public static final String CSVUSUARIO = "/mnt/hdd/code/DadosUsuario.csv";
-    public static final String CSVALIMENTOS = "/mnt/hdd/code/DadosAlimentos.csv";
-    public static void main(String[] args) throws FileNotFoundException {
+    public static final String CSVUSUARIO = "dados/DadosUsuario.csv";
+    public static final String CSVALIMENTOS = "dados/DadosAlimentos.csv";
+    public static final String CSVBASEDIR = "dados";
+    public static final String CSVLOGDIR = "dados/alimento_log";
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+        ArquivoOps aq = new ArquivoOps();
+        aq.checarPrimeiraExecucao();
+
+
 
         
-        InterfaceCLI intf = new InterfaceCLI();
-        intf.mostrar();
+        //InterfaceCLI intf = new InterfaceCLI();
+        // intf.mostrar();
         
     }
 }
